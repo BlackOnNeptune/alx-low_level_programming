962794fa46c4f7cb901d8d2313798a05facea3b6
BlackOnNeptune <ayoubmachhour01@gmail.com>
2023-06-26 05:26:39 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
