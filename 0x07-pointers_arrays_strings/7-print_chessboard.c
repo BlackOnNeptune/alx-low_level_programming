298dd283a114dbd3c7a10be230d4a88719287c21
BlackOnNeptune <ayoubmachhour01@gmail.com>
2023-07-03 15:11:19 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,18 +3,18 @@
 /**
  * print_chessboard - Entry point
  * @a: array
- * Return: Void
+ * Return: Always 0 (Success)
  */
 
 void print_chessboard(char (*a)[8])
 {
 	int i;
-	int j;
+	int n;
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
+		for (n = 0; j < 8; n++)
+			_putchar(a[i][n]);
 		_putchar('\n');
 	}
 }
