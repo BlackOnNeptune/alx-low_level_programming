2a690af4aa636de92ac34f445a11b101f4ab4e84
BlackOnNeptune <ayoubmachhour01@gmail.com>
2023-07-05 05:43:21 -0700

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,37 +1,33 @@
 #include "main.h"
 
 /**
- * wildcmp - Compares two strings and checks for identity.
- * @s1: First string to compare.
- * @s2: Second string to compare.
+ * str_checker - check if two strings are identical.
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index. (joker)
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
+int str_checker(char *s1, char *s2, int i, int j)
+{
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
+}
+/**
+ * wildcmp - check if strings could be considered identical
+ * @s1: base address for string.
+ * @s2: base address for string.
  *
- * Return: 1 if strings are considered identical, 0 otherwise.
+ * Return: 1 if are considered identical.
  */
 int wildcmp(char *s1, char *s2)
 {
-	/* If both strings are empty, they are identical */
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	/* If the characters match or s2 has a wildcard '*' */
-	if (*s1 == *s2 || *s2 == '*')
-	{
-		/* Move to the next characters in both strings */
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-
-	/* If s2 has a wildcard '*', move to the next character in s2 */
-	if (*s2 == '*')
-	{
-		/* Check for identity by considering '*' as an empty string */
-		if (*s1 == '\0')
-			return (wildcmp(s1, s2 + 1));
-		/* Try to match s1 with the rest of s2 */
-		if (wildcmp(s1 + 1, s2))
-			return (1);
-	}
-
-	/* Strings are not identical */
-	return (0);
+	return (str_checker(s1, s2, 0, 0));
 }
-
