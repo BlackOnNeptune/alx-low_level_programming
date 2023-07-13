fdabe65da951ab7cd94f165700782c6aa5072a2e
BlackOnNeptune <ayoubmachhour01@gmail.com>
2023-07-13 05:03:08 -0700

    Update Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,39 +2,49 @@
 #include "main.h"
 
 /**
- * _realloc - Reallocates a memory block using malloc and free
- * @ptr: Pointer to the memory block to reallocate
- * @old_size: Size of the allocated space for ptr
- * @new_size: New size of the memory block
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
  *
- * Return: Pointer to the reallocated memory block, or NULL on failure
+ * Return: pointer to the newly allocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
 
-	if (ptr == NULL)
-{
-	new_ptr = malloc(new_size);
-	return (new_ptr);
-}
-	if (new_size == 0)
-{
-	free(ptr);
-	return (NULL);
-}
 	if (new_size == old_size)
-	return (ptr);
+		return (ptr);
 
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-	return (NULL);
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
 
 	if (new_size < old_size)
-	old_size = new_size;
-	memcpy(new_ptr, ptr, old_size);
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
 
 	free(ptr);
-
-	return (new_ptr);
+	return (ptr1);
 }
