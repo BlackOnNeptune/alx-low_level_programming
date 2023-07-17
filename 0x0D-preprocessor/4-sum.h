37eb5bbf251c2d5ebebaa7f457fd7fa8c6a558a5
BlackOnNeptune <ayoubmachhour01@gmail.com>
2023-07-17 04:22:40 -0700

    0x0D.c

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - Macro to compute the sum of two values
+ * @x: First value
+ * @y: Second value
+ *
+ * Description: This macro computes the sum of two values.
+ * It takes two arguments (x and y) and returns their sum.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
