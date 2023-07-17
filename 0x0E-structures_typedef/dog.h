497c2c52cc5dbe2b3016665fa5aa5f518b58ae6b
BlackOnNeptune <ayoubmachhour01@gmail.com>
2023-07-17 05:10:55 -0700

    Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,73 +9,23 @@
  *
  * Description: Longer description
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 /**
- * init_dog - Initializes a dog structure
- * @d: Pointer to the dog structure
- * @name: Dog's name
- * @age: Dog's age
- * @owner: Dog's owner
- *
- * Description: This function initializes the members of a dog structure.
+ * dog_t - typedef for struct dog
  */
-void init_dog(dog_t *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
 
-/**
- * print_dog - Prints the details of a dog structure
- * @d: Pointer to the dog structure
- *
- * Description: This function prints the name, age, and owner of a dog.
- */
-void print_dog(const dog_t *d);
-
-/**
- * new_dog - Creates a new dog structure
- * @name: Dog's name
- * @age: Dog's age
- * @owner: Dog's owner
- *
- * Return: Pointer to the newly created dog structure
- *
- * Description: This function creates a new dog structure
- *              and initializes its members.
- */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-
-/**
- * free_dog - Frees the memory allocated for a dog structure
- * @d: Pointer to the dog structure
- *
- * Description: This function frees the memory allocated for a dog structure.
- */
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
-/**
- * _strcpy - Copies a string to a destination buffer
- * @dest: Pointer to the destination buffer
- * @src: Pointer to the source string
- *
- * Return: Pointer to the destination buffer
- *
- * Description: This function copies a string from source to destination.
- */
-char *_strcpy(char *dest, const char *src);
-
-/**
- * _strlen - Computes the length of a string
- * @s: Pointer to the string
- *
- * Return: Length of the string
- *
- * Description: This function calculates the length of a string.
- */
-int _strlen(const char *s);
-
-#endif /* DOG_H */
-
+#endif
